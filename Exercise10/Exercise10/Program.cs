27df5dc1e66ab6347bcecadfc6a9fd8d855b9a76
di7chro <crille@crille.org>
2015-06-18 11:24:49 +0200

    Exercise10 started

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitForInput();
+        }
+
+        private static void WaitForInput()
+        {
+            Console.WriteLine("\nPress a key...");
+            Console.ReadLine();
+        }
+
+    }
+}
